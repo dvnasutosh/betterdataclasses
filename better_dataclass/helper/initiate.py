6e4dee679d78c1e34065fe697610a02feda447ae
Asutosh Rath <dvnasutosh@gmail.com>
2023-05-18 17:20:29 +0530

    added OPtional Data handling in initiate

--- better_dataclass/helper/initiate.py
+++ better_dataclass/helper/initiate.py
@@ -6,10 +6,11 @@ from typing import Any, Literal, Union, Optional, Tuple, List, Set, Dict, Type, 
 def initialize(annot: Type) -> Any:
     
     # handling Union
+    if get_origin(annot)== Optional or type(None) in get_args(annot):
+        return None
+    
     if get_origin(annot) == Union:
         return initialize(get_args(annot)[0])
-    elif get_origin(annot)== Optional:
-        return None
     
     elif get_origin(annot) in (List,list):
         return []
@@ -21,7 +22,7 @@ def initialize(annot: Type) -> Any:
         return set()
     
     elif get_origin(annot) in (Tuple,tuple):
-        return tuple()
+        return ()
 
     elif Literal in (get_origin(annot), annot):
         return next(iter(get_args(annot)))
@@ -31,7 +32,6 @@ def initialize(annot: Type) -> Any:
 
     elif annot is Any:
         return None
-
     try:
         return annot()
     except TypeError as e:
@@ -39,47 +39,4 @@ def initialize(annot: Type) -> Any:
 
 
 # Test Cases
-
-# Case 1: Basic initialization
-print(initialize(int))              # Expected: 0
-print(initialize(str))              # Expected: ''
-print(initialize(bool))             # Expected: False
-print(initialize(float))            # Expected: 0.0
-
-# Case 2: Union initialization
-print(initialize(Union[int, str]))         # Expected: 0
-print(initialize(Union[str, bool]))        # Expected: ''
-print(initialize(Union[float, int, bool])) # Expected: 0.0
-
-# Case 3: List initialization
-print(initialize(List[int]))        # Expected: []
-print(initialize(List[str]))        # Expected: []
-print(initialize(List[bool]))       # Expected: []
-
-# Case 4: Dict initialization
-print(initialize(Dict[str, int]))   # Expected: {}
-print(initialize(Dict[int, bool]))  # Expected: {}
-
-# Case 5: Set initialization
-print(initialize(Set[str]))         # Expected: set()
-print(initialize(Set[int]))         # Expected: set()
-
-# Case 6: Tuple initialization
-print(initialize(Tuple[int, str]))  # Expected: (0, '')
-print(initialize(Tuple[str, int]))  # Expected: ('', 0)
-
-# Case 7: Literal initialization
-print(initialize(Literal[1, 2, 3]))  # Expected: 1
-
-# Case 8: Enum initialization
-class Color(enum.Enum):
-    RED = 1
-    GREEN = 2
-    BLUE = 3
-
-print(initialize(Color))                    # Expected: Color.RED
-
-# Case 9: Any initialization
-print(initialize(Any))                # Expected: None
-
 
