4673c5450865a386a6e644eb64ebe10a3467d674
Asutosh Rath <dvnasutosh@gmail.com>
2023-05-20 12:45:12 +0530

    Added Required details for setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+    name="betterdataclass",
+    version="2.01",
+    description="A multipurpose dataclass libarary used for validation and data structuring.",
+    long_description="`betterdataclass` is a Python library that enhances the functionality of the existing `dataclass` system. It provides additional features and improvements to make working with data classes even better.",
+    author="Asutosh Rath",
+    author_email="dvnasutosh@gmail.com",
+    packages=['betterdataclass'],
+        classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ]
+)
